e8a5f262fe26d6ddae7cffc7e79fb1624975c095
Jiadong.Yu <jiadong@pandu.ie>
2018-12-20 22:32:09 +0800

    add another pages

--- config/vendors.js
+++ config/vendors.js
@@ -11,7 +11,9 @@ module.exports = {
     javascripts: [
         'node_modules/jquery/dist/jquery.js',
         'node_modules/swiper/dist/js/swiper.jquery.umd.js',
-        'config/vue.js'
+        // 'node_modules/md5.js/index.js',
+        'config/md5.js',
+        'config/vue.js',
     ],
 
     fonts: [
